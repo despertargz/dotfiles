62cd3aaece7e8e6da73871d3ca3c1cce683075ad
chris <chris@DESKTOP-CVAGRB0.localdomain>
2023-04-17 21:57:38 -0500

    new

---
+++ lua/bufferline-conf.lua
@@ -0,0 +1,16 @@
+    require('bufferline').setup {
+		options = {
+			mode = 'tabs',
+			buffer_close_icon=''
+		},
+        highlights = {
+				buffer_selected = { italic = false },
+--				diagnostic_selected = { italic = false },
+--				hint_selected = { italic = false },
+--				pick_selected = { italic = false },
+--				pick_visible = { italic = false },
+--				pick = { italic = false },
+            }
+        }
+    
+
